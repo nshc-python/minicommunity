f218c047bd06c81b42e86f3905f6cf381e80042e
binsoopark <binsoopark@gmail.com>
2015-07-01 21:36:17 +0900

    PYFLASK-6, PYFLASK-9
    회원가입시 유효성을 체크한다. 
    회원가입을 할 수 있다.

--- minicommunity/controller/register_member.py
+++ minicommunity/controller/register_member.py
@@ -16,7 +16,8 @@ from wtforms.fields.simple import TextField, PasswordField, HiddenField
 from wtforms import validators
 from werkzeug.security import generate_password_hash
 from werkzeug.utils import redirect
-from sqlalchemy.types import DateTime
+#from sqlalchemy.types import DateTime
+from datetime import datetime
 
 
 @minicommunity.route('/member/register')
@@ -33,16 +34,20 @@ def register_member():
     
     rform = RegisterForm(request.form)
 
-    Log.debug('aaa')    
-    
     if rform.validate():
-        email = rform.email.data
-        nickname = rform.nickname.data
-        password = rform.password.data
-#         password_confirm = form.password_confirm.data
+        email = rform.r_email.data
+        nickname = rform.r_nickname.data
+        password = rform.r_password.data
         
+        Log.debug('r_email : '+str(rform.r_email.data))    
+        Log.debug('r_nickname : '+str(rform.r_nickname.data))    
+        Log.debug('r_password_o : '+str(rform.r_password.data))    
+        Log.debug('r_password_c : '+str(rform.r_password_confirm.data))
+        Log.debug('r_email_check : '+str(rform.r_email_check.data))
+        Log.debug('r_nickname_check : '+str(rform.r_nickname_check.data))
+            
         try:
-            member = Member(email, generate_password_hash(password), nickname, DateTime.datetime())
+            member = Member(email, generate_password_hash(password), nickname, datetime.today(), datetime.today())
             dao.add(member)
             dao.commit()
             
@@ -60,18 +65,30 @@ def register_member():
                                     register_member_name=nickname))
             
     else:
-        return redirect(url_for('minicommunity.login', form=rform))
+        return redirect(url_for('.login', form=rform))
     
 
 @minicommunity.route('/member/check_email', methods=['POST'])
 def check_email():
     email = request.json['email']
     
-    Log.debug('email : ')
-    Log.debug(email)
+    Log.debug('email : '+email)
     
     #: DB에서 email 중복 확인 
     if __get_member(email) :
+        return jsonify(result = False)
+    else:
+        return jsonify(result = True)
+    
+
+@minicommunity.route('/member/check_nickname', methods=['POST'])
+def check_nickname():
+    nickname = request.json['nickname']
+    
+    Log.debug('nickname : '+nickname)
+    
+    #: DB에서 nickname 중복 확인 
+    if __get_member_from_nickname(nickname) :
         return jsonify(result = False)
     else:
         return jsonify(result = True)
@@ -88,11 +105,23 @@ def __get_member(email):
     except Exception as e:
         Log.error(str(e))
         raise e
+    
+def __get_member_from_nickname(nick):
+    try:
+        current_member = dao.query(Member) \
+                            .filter_by(nickname=nick) \
+                            .first()
+        
+        Log.debug(current_member)
+        return current_member
+    except Exception as e:
+        Log.error(str(e))
+        raise e
             
 
 class RegisterForm(Form):
     # 사용자 등록화면에서 입력값을 검증
-    email = TextField('Email',
+    r_email = TextField('Email',
                       [validators.Required('Email을 입력하세요.'),
                        validators.Length(
                                          min=5,
@@ -100,26 +129,26 @@ class RegisterForm(Form):
                                          message='5자 이상 40자 이하로 입력하세요.'),
                        validators.Email(message='이메일 형식에 맞지 않습니다.')])
     
-    nickname = TextField('Nickname',
+    r_nickname = TextField('Nickname',
                          [validators.Required('화면에 표시될 닉네임을 입력하세요.'),
                           validators.Length(
                                             min=2,
                                             max=30,
                                             message='2자이상 30자 이하로 입력하세요.')])
     
-    password = PasswordField('New password',
+    r_password = PasswordField('New password',
                              [validators.Required('비밀번호를 입력하세요.'),
                               validators.Length(
                                                 min=4,
                                                 max=20,
                                                 message='4자이상 20자 이하로 입력하세요.'),
-                              validators.EqualTo('password_confirm',
+                              validators.EqualTo('r_password_confirm',
                                                  message='비밀번호가 일치하지 않습니다.')])
     
-    password_confirm = PasswordField('Confirm password')
+    r_password_confirm = PasswordField('Confirm password')
     
-    email_check = HiddenField('Check email',
+    r_email_check = HiddenField('Check email',
                                  [validators.Required('중복되는 이메일이 있습니다.')])
     
-    nickname_check = HiddenField('Check nickname',
+    r_nickname_check = HiddenField('Check nickname',
                                  [validators.Required('중복되는 닉네임이 있습니다.')])
